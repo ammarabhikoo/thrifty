cb5411cf4098e9ff7e9cf959fa654f09eabf9054
ac111158 <ac111158@8CG8163KX3>
2023-03-08 12:24:44 +1300

    Add project files.

---
+++ tpi website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tpi_website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
