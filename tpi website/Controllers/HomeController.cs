0ed28450934fd64b414547b05d1b972500461fc8
Ammara Bhikoo <ac111158@avcol.school.nz>
2023-04-05 12:58:01 +1200

    changed carosel to bootstrap- fixed glitch. added newsletter subscription

--- tpi website/Controllers/HomeController.cs
+++ tpi website/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace tpi_website.Controllers
             return View();
         }
 
+        public IActionResult Wishlist()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
