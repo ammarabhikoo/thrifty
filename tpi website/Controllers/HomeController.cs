082ea70b62ffd4c93aa60016c805e2c8c01e9d61
Ammara Bhikoo <ac111158@avcol.school.nz>
2023-04-06 09:58:10 +1200

    added icons to navbar

--- tpi website/Controllers/HomeController.cs
+++ tpi website/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace tpi_website.Controllers
             return View();
         }
 
+        public IActionResult Bag()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
