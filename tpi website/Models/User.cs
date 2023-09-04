8504e95b834fdcf89e76946038aaa198653b8074
Ammara Bhikoo <ac111158@avcol.school.nz>
2023-09-04 13:45:04 +1200

    made changes

--- tpi website/Models/User.cs
+++ tpi website/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using System.ComponentModel.DataAnnotations;
 using System.Data;
+using System.Net.NetworkInformation;
 using System.Xml.Linq;
 
 namespace tpi_website.Models
@@ -9,12 +10,16 @@ namespace tpi_website.Models
     {
         
         public int Id { get; set; }
-        public string FirstName { get; set; }
+
         [Display(Name = "First Name")]
-        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
         [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Phone Number")]
         public int Phone { get; set; }
-        [Display(Name = "Product Number")]
+
         public string Email { get; set; }
 
         public int Age { get; set; }
