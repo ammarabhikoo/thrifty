ba0245b15f6b00e26d9a473d259c0d6dd03a0c31
Ammara Bhikoo <ac111158@avcol.school.nz>
2023-10-07 19:27:52 +1300

    worked on footer

---
+++ tpi website/Views/Home/returns.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace tpi_website.Views.Home
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
