ee0dd043f2adc056a3f851d4b323c818e5ef8d54
Ammara Bhikoo <ac111158@avcol.school.nz>
2023-10-17 22:10:40 +1300

    added pop up for discount

--- tpi website/wwwroot/js/site.js
+++ tpi website/wwwroot/js/site.js
@@ -51,4 +51,42 @@ function toggleAccordion() {
 }
 items.forEach((item) => item.addEventListener('click', toggleAccordion));
 
+document.addEventListener('DOMContentLoaded', function () {
+    const popupContainer = document.getElementById('popup-container');
+    const closeBtn = document.getElementById('close-popup');
+    const emailInput = document.getElementById('email');
+    const submitButton = document.getElementById('submit-email');
+    const emailError = document.getElementById('email-error'); 
+
+    // Show the pop-up on page load
+    popupContainer.style.display = 'flex';
+
+    // Close the pop-up when the close button is clicked
+    closeBtn.addEventListener('click', function () {
+        popupContainer.style.display = 'none';
+    });
+
+    // Handle email submission
+    submitButton.addEventListener('click', function () {
+        const userEmail = emailInput.value;
+
+        // Basic email validation using a regular expression
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+        if (!emailPattern.test(userEmail)) {
+            emailInput.setCustomValidity(''); // Clear any previous custom error message
+            emailInput.setCustomValidity('Invalid email address'); // Set the custom error message
+            emailError.textContent = 'Error: Invalid email address entered'; // Display the error message
+            emailInput.reportValidity(); // Display the error message in the browser
+        } else {
+            emailInput.setCustomValidity(''); // Clear the custom error message if the email is valid
+            emailError.textContent = ''; // Clear the error message
+            alert(`Thank you! You submitted: ${userEmail}`);
+            popupContainer.style.display = 'none';
+        }
+    });
+});
+
+
+
 
